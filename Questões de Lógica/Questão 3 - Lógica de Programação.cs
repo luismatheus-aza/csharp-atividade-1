b7329fbd997efe0d81511edaef1ae5d0e09d7111
luismatheus-aza <luis.gutierrez@ba.estudante.senai.br>
2026-06-26 20:50:53 -0300

    Add files via upload

---
+++ Questões de Lógica/Questão 3 - Lógica de Programação.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Questão 3 de Lógica de Programação 26/06/2026
+        double[] numeros = new double[3];
+        for (int count = 0; count < numeros.Length; count++)
+        {
+            Console.Write($"Insira a {count + 1}º nota: ");
+            numeros[count] = double.Parse(Console.ReadLine());
+        }
+
+        double media = numeros.Average();
+        Console.WriteLine($"\nMédia: {media:F2}");
+        if (media >= 6.9)
+        {
+            Console.WriteLine("Situação: Aprovado!");
+        }
+        else if (media >= 5.0)
+        {
+            Console.WriteLine("Situação: Recuperação!");
+        }
+        else
+        {
+            Console.WriteLine("Situação: Reprovado!");
+        }
+    }
+}
---
+++ Questões de Lógica/Questão 5 - Lógica de Programação.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Questão 5 de Lógica de Programação 26/06/2026
+        Console.WriteLine("Insira o número: ");
+        short numero = short.Parse(Console.ReadLine());
+
+        for (int count = 0; count <= 10; count++)
+        {
+            Console.WriteLine($"{numero} x {count} = {numero*count} ");
+        }
+
+    }
+}
