3e61d181856f66e1510fac85b7ea070401c00de5
luismatheus-aza <luis.gutierrez@ba.estudante.senai.br>
2026-06-26 20:14:13 -0300

    Add files via upload

---
+++ Questões de Lógica/Questão 2 - Lógica de Programação.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Questão 2 de Lógica de Programação 26/06/2026
+        short[] idades = new short[1];
+
+        for (int count = 0; count < idades.Length; count++)
+        {
+            Console.WriteLine($"- - - Insira a {count + 1}ª idade - - -");
+            idades[count] = short.Parse(Console.ReadLine());
+
+            if (idades[count] >= 18)
+            {
+                Console.WriteLine("Maior de Idade");
+            }
+            else
+            {
+                Console.WriteLine("Menor de Idade");
+            }
+        }
+    }
+}
+
+
